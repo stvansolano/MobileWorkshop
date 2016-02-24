6207dce86b9807d81aff853009a260f85a611853
stvansolano <stvansolano@gmail.com>
2016-02-23 21:56:18 -0600

    Added Stepper and Picker controls

--- MobileWorkshop/MobileWorkshop/Pages/BasicControlsPage.xaml.cs
+++ MobileWorkshop/MobileWorkshop/Pages/BasicControlsPage.xaml.cs
@@ -2,17 +2,22 @@
 {
     using Xamarin.Forms;
     using System.Windows.Input;
+	using System.Collections.Generic;
 
     public partial class BasicControlsPage
     {
         public BasicControlsPage()
-        {
-            MessageCommand = new Command(SayMessage);
+		{
+			MessageCommand = new Command (SayMessage);
 
-            InitializeComponent();
+			InitializeComponent ();
 
-            BindingContext = this;
-        }
+			PickerControl.Items.Add("Item 1");
+			PickerControl.Items.Add("Item 2");
+			PickerControl.Items.Add("Item 3");
+
+			BindingContext = this;
+		}
 
         private async void SayMessage()
         {
