964f7e349a8aac9c9265dbca5ea33fd8355c456f
stvansolano <stvansolano@outlook.com>
2020-08-18 13:07:44 -0600

    Part 3: Customizing the shell

--- src/MobileWorkshop/App.xaml.cs
+++ src/MobileWorkshop/App.xaml.cs
@@ -3,30 +3,76 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using MobileWorkshop.Services;
 using MobileWorkshop.Views;
+using System.Linq;
 
 namespace MobileWorkshop
 {
 	public partial class App : Application
 	{
-
 		public App()
 		{
 			InitializeComponent();
 
 			DependencyService.Register<MockDataStore>();
-			MainPage = new AppShell();
+			MainPage = new CustomShell();
+		}
+	}
+
+	public class CustomShell : Shell
+	{
+		public CustomShell() => Build();
+		public ItemsPage AllItems { get; set; } = new ItemsPage();
+		public AboutPage About { get; set; } = new AboutPage();
+
+		private void Build()
+		{
+			FlyoutBehavior = FlyoutBehavior.Flyout;
+
+			FlyoutHeader = new HeaderView();
+
+			AddFlyout("Home", AllItems, "My to-dos");
+			AddFlyout("About", new AboutPage());
+
+			AddTab("Done", new ContentPage { Content = new Label { Text = "Done!" } });
+
+			CurrentItem = Items.First();
 		}
 
-		protected override void OnStart()
+		private void AddFlyout(string title, ContentPage page, string defaultTab = null)
 		{
+			defaultTab = defaultTab ?? title;
+
+			var flyout = new FlyoutItem
+			{
+				Title = title,
+				Items = {
+					new Tab {
+						Title = defaultTab,
+						Items = {
+							new ShellContent {
+								ContentTemplate = new DataTemplate(() => page)
+							}
+						}
+					}
+				}
+			};
+			Items.Add(flyout);
 		}
 
-		protected override void OnSleep()
+		private void AddTab(string title, ContentPage page)
 		{
-		}
-
-		protected override void OnResume()
-		{
+			var flyoutItem = Items.OfType<FlyoutItem>().FirstOrDefault();
+			
+			flyoutItem.Items.Add(
+				new Tab
+				{
+					Title = title,
+					Items = {
+							new ShellContent {
+								ContentTemplate = new DataTemplate(() => page)
+							}
+						}
+				});
 		}
 	}
 }
---
+++ src/MobileWorkshop/Views/HeaderView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace MobileWorkshop.Views
+{
+	public partial class HeaderView : ContentView
+	{
+		public HeaderView()
+		{
+			InitializeComponent();
+		}
+	}
+}
